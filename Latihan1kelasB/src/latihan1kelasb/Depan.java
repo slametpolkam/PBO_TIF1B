d5ef69814a7192eb139244fe01049090d82c64ae
slametpolkam <slamet@poltek-kampar.ac.id>
2021-06-08 11:21:51 +0700

    Latihan 1 Kelas 1 B

---
+++ Latihan1kelasB/src/latihan1kelasb/Depan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1kelasb;
+
+/**
+ *
+ * @author oryza
+ */
+public class Depan {
+    public static void main(String args[]){
+        Operator cek=new Operator();
+        System.out.println(cek.penjumlahan(4, 6));
+    }
+}
